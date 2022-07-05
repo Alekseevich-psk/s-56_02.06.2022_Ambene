45e40653b336803645895b24bf9409e1c3043a83
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2022-07-05 23:01:16 +0300

    add youtube video for product section

--- app/scripts/product-video.js
+++ app/scripts/product-video.js
@@ -4,30 +4,16 @@
 
     const videoBG = parent.querySelector(".product__video-bg");
     const videoWrap = parent.querySelector(".product__video-wrap");
-    const videoPlayer = parent.querySelector(".product__video-wrap video");
+    const videoPlayer = parent.querySelector("#product-video");
 
-    let flagPlay = true;
-
-    if(videoBG && videoWrap && videoPlayer) {
+    if (videoBG && videoWrap && videoPlayer) {
         parent.addEventListener("click", function (e) {
             let target = e.target;
-    
+
             if (target.classList.contains("product__video-bg")) {
                 videoBG.classList.add("hide");
                 videoWrap.classList.remove("hide");
-                videoPlayer.play();
-            }
-        });
-    
-        videoWrap.addEventListener("click", () => {
-            if (flagPlay) {
-                videoPlayer.pause();
-                flagPlay = false;
-                console.log(flagPlay);
-            } else {
-                videoPlayer.play();
-                flagPlay = true;
-                console.log(flagPlay);
+                videoPlayer.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
             }
         });
     }
